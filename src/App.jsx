16ed35950087d3f0aa78d947fcd1938a1fd0b855
jogabeca <jogabeca@gmail.com>
2026-07-14 23:03:06 -0500

    exercise 1.6 Unicafe

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,28 @@
-import React from 'react'
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      {/* Enviamos solo la propiedad name para el título */}
-      <Header course={course.name} /> 
+      <h1>give feedback</h1>
       
-      {/* Enviamos el arreglo completo que está adentro de course */}
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {/* handler directly in the button */}
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      
+      <h1>statistics</h1>
+      
+      {/* show numbers */}
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
     </div>
-  ) 
+  )
 }
 
 export default App

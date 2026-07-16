e96cbb7b5980967f207879443d8d5542065ffac1
jogabeca <jogabeca@gmail.com>
2026-07-15 22:07:10 -0500

    exercise 1.12 anecdotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,32 @@
-import React from 'react'
-
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-    </p>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are by definition not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  // state to store the index of the selected anecdote
+  const [selected, setSelected] = useState(0)
+
+  // function to generate a random index and update state
+  const handleNextClick = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
   }
 
   return (
     <div>
-      {/* Enviamos solo la propiedad name para el título */}
-      <Header course={course.name} /> 
-      
-      {/* Enviamos el arreglo completo que está adentro de course */}
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleNextClick}>next anecdote</button>
     </div>
-  ) 
+  )
 }
 
 export default App
